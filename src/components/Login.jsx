f2240a362ef12702ad8de6fb6c2ba8cd89aebe66
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-04-18 22:08:07 +0530

    feat: implement login component with state management and async submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAuth } from "./hooks/useAuth";
 
 export const Login = () => {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  const { login } = useAuth();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await login(credentials);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-    <h1>Login Form</h1>
-    <div>
+      <h1>Login Form</h1>
+      <div>
         <label htmlFor="">Email</label>
-        <input type="email" name="" id="" />
+        <input
+          type="email"
+          name="email"
+          value={credentials.email}
+          onChange={handleChange}
+        />
         <label htmlFor="">Password</label>
-        <input type="password" name="" id="" />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button type="submit" onClick={handleSubmit}>Login</button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
