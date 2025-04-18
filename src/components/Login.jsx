99cad50164f35f9c8d885b8a4f6a7a0de6abbe74
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-04-18 21:56:58 +0530

    feat: add initial login component with email and password fields

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div>
+    <h1>Login Form</h1>
+    <div>
+        <label htmlFor="">Email</label>
+        <input type="email" name="" id="" />
+        <label htmlFor="">Password</label>
+        <input type="password" name="" id="" />
+    </div>
+    </div>
+  )
+}
