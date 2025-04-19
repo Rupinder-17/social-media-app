0265aff60c5a838743dd6709416e189a7a9d73f0
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-04-19 15:11:50 +0530

    feat: implement Login and Register components with state management and routing

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useAuth } from "./components/hooks/useAuth";
+import { useAuth } from "../hooks/useAuth";
 import { Navigate, useNavigate } from "react-router-dom";
 
 export const Login = () => {
