0b1cb5072f2e355246f4dcc5fe6a1334b04e8cca
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-04-22 22:03:36 +0530

    refactor(VeiwPosts): clean up formatting and improve readability

--- src/components/pages/VeiwPosts.jsx
+++ src/components/pages/VeiwPosts.jsx
@@ -1,18 +1,26 @@
-import React, { useEffect } from 'react'
-import { usePost } from '../hooks/usePost'
+import React, { useEffect } from "react";
+import { usePost } from "../hooks/usePost";
 
 export const VeiwPosts = () => {
-     const {posts, PostGet} =usePost()
-     console.log("postfetch", posts);
-     
-     useEffect(()=>{
-        PostGet()
-     },[])
+  const { posts, PostGet } = usePost();
+  console.log("postfetch", posts);
+
+  useEffect(() => {
+    PostGet();
+  }, []);
   return (
     <div>
-        <h1>your posts</h1>
-        {/* {posts.data.posts.content} */}
-
+      <h1>your posts</h1>
+      {posts?.data?.posts.length > 0 ? (
+        posts?.data?.posts.map((post, index) => (
+          <div>
+            <p key={index}>{post.content}</p>
+            <img src={post.image} alt="" />
+          </div>
+        ))
+      ) : (
+        <p>no posts found</p>
+      )}
     </div>
-  )
-}
+  );
+};
