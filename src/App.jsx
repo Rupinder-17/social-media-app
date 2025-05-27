c00e0f2b5f3c6b3521de42094332d949e460fd5f
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-05-27 21:38:58 +0530

    feat: remove Form and CountryForm components, update App to use Index

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { Index } from "./components/pages/Index"
-import { CountryForm } from "./countrycomponent/CountryForm"
-// import { Form } from "./Form"
 
 function App() {
   return(
     <>
     {/* <Register/> */}
-    {/* <Index/> */}
-    {/* <Form/> */}
-    <CountryForm/>
+    <Index/>
     </>
   )
   
